6e9dc985b9e0c2f2bf807033263cf3e22f80b533
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-02-05 21:57:33 -0800

    Added more Error checking capabilities. Affected: index.html, index.css, index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 $( document ).ready( function() {
-	console.log('ready');
+	initHides();
 	$("#alert").hide();
-	$("#errorN").hide();
 	$(".date").datepicker();
+
+	var message = "Thanks "
 
 	//Clicking Ok hides alert
 	$("button").click( function() {
@@ -12,14 +13,63 @@ $( document ).ready( function() {
 	//
 	$("#check").click( function() {
 		if ( !$("#from").val() || !$("#to").val() || !$("#name").val() ) {
-			console.log("no");
-			$("#errorN").show();
+			checkBlanks();
+		} else if ( invalidDate() ) {
+			checkBlanks();
 		} else {
-			console.log("yes");
-			$("#errorN").hide();
-			$("#alert").show();
+			initHides();
+			checkBlanks();
+			confirmDate();
 		}
 
 		return false;
 	})
+
+	//NecessaryHides() just to keep it clean.
+	function initHides() {
+		$("#errorF").hide();
+		$("#errorT").hide();
+		$("#errorN").hide();
+		$("#invalidDate").hide();
+	}
+
+	//Creates a message confirming the date
+	function confirmDate() {
+		createMessage();
+		$("#text").html( message );
+		$("#alert").show();
+	}
+	//Creates the message for above function
+	function createMessage() {
+		message = "<p>Thanks "+$("#name").val()+"! Your trip leaves on "+$("#from").val()+" and returns on "+$("#to").val()+"!</p>";
+	}
+
+	//Validate Date
+	function invalidDate() {
+		if ( $("#to").val() < $("#from").val() ) {
+			console.log("Disallow");
+			$("#invalidDate").show();
+			return true;
+		}
+		else {
+			$("#invalidDate").hide();
+			return false;
+		}
+	}
+
+	//Checks input fields for blanks
+	function checkBlanks() {
+		checkBlank( "#from", "#errorF" );
+		checkBlank( "#to", "#errorT" );
+		checkBlank( "#name", "#errorN" );
+	}
+	function checkBlank( check, err ) {
+		if ( !$( check ).val() ) {
+			$( err ).show();
+			$( check ).addClass("err");
+		} else {
+			$( err ).hide();
+			$( check ).removeClass("err");
+		}
+	}
 })
